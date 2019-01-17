84bad79be37cf6b3fc4edc4cf3c0c891f377e804
vt <vt@anadeainc.com>
2019-01-17 13:27:56 +0300

    add semicolons

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
-  console.log(`Micro-service on port ${port}`)
-})
+  console.log(`Micro-service on port ${port}`);
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello')
-})
+  res.send('Hello');
+});
 
 app.get('/hello/:slug', (req, res) => {
-  res.send(`Hello ${req.params.slug}`)
-})
+  res.send(`Hello ${req.params.slug}`);
+});
 
 app.post('/post', (req, res) => {
-  res.send(JSON.stringify(req.body))
-})
+  res.send(JSON.stringify(req.body));
+});
